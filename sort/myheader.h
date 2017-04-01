7e19e73e1e0723b5f37fa240dea516170851a92f
杨悦 <645082935@qq.com>
2017-04-01 14:37:55 +0800

    修改：     sort/myheader.h

--- sort/myheader.h
+++ sort/myheader.h
@@ -2,8 +2,8 @@
  *file: myheader.h
  */
 
-#ifndef MY_HEADER.H
-#define MY_HEADER.H
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
 /* 计算数组大小 */
 #define GetSizeOfArray(array, len) len = sizeof(array) / sizeof(array[0])
 #endif
