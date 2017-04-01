82694ccea6b3a104e466bf61647a59b3fb73aebd
杨悦 <645082935@qq.com>
2017-04-01 18:08:54 +0800

    新文件：   Map.js

---
+++ Map.js
@@ -0,0 +1,22 @@
+function getParent(node) {
+  var parent = node.
+}
+
+function reformTree(node) {
+
+}
+
+function Map() {
+  Map.prototype.Entry = function() {
+    var key;
+    var value;
+  }
+  var array = [];
+  function get(var key) {
+    for(array)
+  }
+
+  Map.prototype.put = function(key, value) {
+    array.put
+  }
+}
