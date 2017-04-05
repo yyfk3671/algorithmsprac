881c8e9702174b20087ba846067a5d93f783ca39
yangyue <yangyue@yangyuedeMacBook-Pro.local>
2017-04-05 22:46:54 +0800

    modified:   sort/insertsort/insertsort.c

--- sort/insertsort/insertsort.c
+++ sort/insertsort/insertsort.c
@@ -9,35 +9,35 @@ void insertsort(int*, int);
 
 int main(int argc, char **argv)
 {
-		int array[] = {9, 9, 7, 6, 5, 4, 3, 2, 1 ,0};
-		int size;
-		GetSizeOfArray(array, size);
-		printf("before sort:\n\t");
-		printAry(array, size);
-		insertsort(array, size);
-		printf("after sort:\n\t");
-		printAry(array, size);
-		return 0;
+  int array[] = {9, 9, 7, 6, 5, 4, 3, 2, 1 ,0};
+  int size;
+  GetSizeOfArray(array, size);
+  printf("before sort:\n\t");
+  printAry(array, size);
+  insertsort(array, size);
+  printf("after sort:\n\t");
+  printAry(array, size);
+  return 0;
 }
 
 void insertsort(int *p, int size)
 {
-		int i;
-		int temp;
-		for(i = 1; i < size; i++)
-		{
-				int j = i - 1;
-				temp = p[i];
-				while(j > 0 && temp < p[j]) {
-					p[j+1] = p[j];
-					printf("switch: ");
-					printAry(p, size);
-					j--;
-				}
-				p[j] = temp;
-				printAry(p, size);
-				printf("\n");
+  int i;
+  int temp;
+  for(i = 1; i < size; i++)
+  {
+    int j = i - 1;
+    temp = p[i];
+    while(j > 0 && temp < p[j]) {
+      p[j+1] = p[j];
+      printf("switch: ");
+      printAry(p, size);
+      j--;
+    }
+    p[j] = temp;
+    printAry(p, size);
+    printf("\n");
 
-		}
+  }
 
 }
